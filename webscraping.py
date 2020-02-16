a849ed799ecad6d9d5ce474671f9cbbeaa3524f3
Abdullahalmuhit <abdullahalmuhit1296@gmail.com>
2020-02-16 11:21:03 +0600

    Added as a Function

--- webscraping.py
+++ webscraping.py
@@ -2,46 +2,62 @@ import requests
 import socket
 import dns.resolver
 import tldextract
-
-
-
+import urllib.request
+# for internal pc information
+import getpass
+import os.path
+import os
 
 url = input("Enter Your URL: ")
 info = tldextract.extract(url)
 
-answers = dns.resolver.query(url,'NS')
+def get_status():
+    response = requests.get('https://'+url)
+    status = response.status_code
+    if status == 200:
+        print("Website Exist")
+    else:
+        print("There is a problem on your website")
 
-response = requests.get('https://'+url)
-status = response.status_code
-IP = socket.gethostbyname(url)
-if IP is not None:
-    addr = socket.gethostbyaddr(IP)
-else:
-    print('null')
+def dnfresolver():
+    answers = dns.resolver.query(url,'NS')
+    for server in answers:
+        print (server)
+    
 
-if status == 200:
-    print("Website Exist")
-else:
-    print("There is a problem on your website")
+def get_ip_servername():
+    IP = socket.gethostbyname(url)
+    try:
+        addr = socket.gethostbyaddr(IP)
+        print("Addr  Is:",addr ) 
+    except Exception as e:
+        print(str(e))
+    
+    print("IP  Is:",IP )
+    
+
+
+
+get_status()
+dnfresolver()
+get_ip_servername()
 # print(socket.gethostbyname('cleopatra.io'))
 # print(socket.gethostbyaddr('63.245.208.212'))
 requests.get('https://'+url)
 
-#to get ip address
-print("IP  Is:",IP )
-#to get domain address
-print("Addr  Is:",addr )
-#to get name server
-for server in answers:
-    print (server)
-
 #get domain sub domaine
-
-print("Details:" , info)
 print("Domain Name: ", info.domain)
 print("Subdomain: ", info.subdomain)
 print("Suffix: ", info.suffix)
 print("Domain with suffix: ", info.registered_domain)
 print("Full Domain: ", '.'.join(info))
+
+# for internal pc information
+username = getpass.getuser()
+print(username)
+homedir = os.path.expanduser("~")
+print(homedir)
+
+
 
 
